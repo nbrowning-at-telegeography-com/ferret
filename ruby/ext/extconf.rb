279b7f7f3c29e92788667d8513a12b2a5b450ba7
Nicholas Browning <nbrowning@telegeography.com>
2024-07-12 12:38:54 -0400

    Update extconf.rb adding cflag -Wno-incompatible-pointer-types to Makefile with RUBY_PLATFORM begins with arm64_darwin

--- ruby/ext/extconf.rb
+++ ruby/ext/extconf.rb
@@ -6,10 +6,13 @@ if (/mswin/ =~ RUBY_PLATFORM) and ENV['make'].nil?
 elsif ENV['FERRET_DEV']
   require 'mkmf'
   $CFLAGS = " -g -Wall -fno-stack-protector -fno-common -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=500"
-  puts $CFLAGS
   create_makefile("ferret_ext")
 else
   require 'mkmf'
-  $CFLAGS += " -Wall -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=500"
+  if (/^arm64-darwin/ =~ RUBY_PLATFORM)
+    $CFLAGS += " -Wall -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=500 -Wno-incompatible-pointer-types"
+  else
+    $CFLAGS += " -Wall -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=500"
+  end
   create_makefile("ferret_ext")
 end
